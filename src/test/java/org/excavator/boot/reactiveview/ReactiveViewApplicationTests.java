0eab0febda9a99c6db548c9cc4543e1d70cc1332
CMonkey <42.codemonkey@gmail.com>
2019-05-10 16:07:16 +0800

    add ReactiveViewApplicationTests.testCalcHeight

--- src/test/java/org/excavator/boot/reactiveview/ReactiveViewApplicationTests.java
+++ src/test/java/org/excavator/boot/reactiveview/ReactiveViewApplicationTests.java
@@ -5,12 +5,22 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReactiveViewApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testCalcHeight(){
+	    var chair = new Chair();
+	    var height = chair.calcHeight(Size.S);
+
+	    assertEquals(height, 10);
 	}
 
 }
